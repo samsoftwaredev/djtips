78c30c2da020526df531377fdb56cd878b5ce0a3
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2025-02-26 22:30:42 -0600

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,7 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
+          <li>DJ Tip</li>
         </ol>
 
         <div className={styles.ctas}>
