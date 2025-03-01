891a78d810307d2a6ecc32d833f786807861a1fd
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2025-03-01 02:26:41 -0600

    add analytics to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthContextProvider, ThemeContextProvider } from "@/hooks";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <AuthContextProvider>
           <ThemeContextProvider>{children}</ThemeContextProvider>
         </AuthContextProvider>
+        <Analytics />
       </body>
     </html>
   );
